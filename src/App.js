c0b3f65e55adfc6af3231ad9434a6e9ed7a462a5
Ashley <liweimei0701@gmail.com>
2021-04-15 13:03:20 +1000

    movie search app use hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import "./App.css";
+import Header from "./components/Header";
+import Movie from "./components/Movies";
+import Search from "./components/Search";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+  
+  const getMovies = async (searchValue) => {
+    const res = await axios.get(`https://www.omdbapi.com/?s=${searchValue}&apikey=4b954c0f`);
+    // console.log(res);
+    if (res.data.Response === "True") {
+      setMovies(res.data.Search);
+      setLoading(false);
+    } else {
+      setErrorMessage('sorry can not find it');
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => getMovies('world'),[])
+
+  const search = searchValue => {
+    setLoading(true);
+    setErrorMessage(null);
+    getMovies(searchValue)
+
+  	};
+ 
+    return (
+     <div className="Movie-App">
+      <Header text="SEARCH YOUR FAVOURITE MOVIES" />
+      <Search search={search} />
+      <p className="App-intro">Sharing a few of our favourite movies</p>
+      <div className="movies">
+        {loading && !errorMessage ? (
+         <span>loading...</span>
+         ) : errorMessage ? (
+          <div className="errorMessage">{errorMessage}</div>
+        ) : (
+          movies.map((movie, index) => (
+            <Movie key={`${index}-${movie.Title}`} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
+
 
 export default App;
